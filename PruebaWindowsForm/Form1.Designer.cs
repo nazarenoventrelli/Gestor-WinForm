ca9e8301c43e703db1c7ec703e1a1394ed273990
nazarenoventrelli <nazareno1512@gmail.com>
2018-07-16 12:47:40 -0300

    .

---
+++ PruebaWindowsForm/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace PruebaWindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btn_agregar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_nom = new System.Windows.Forms.TextBox();
+            this.txt_ap = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_edad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cod = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.edad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fechaalta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.personasBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dB_A3E238_PersonasDBDataSet = new PruebaWindowsForm.DB_A3E238_PersonasDBDataSet();
+            this.personasTableAdapter = new PruebaWindowsForm.DB_A3E238_PersonasDBDataSetTableAdapters.PersonasTableAdapter();
+            this.dB_A3E238_PersonasDBDataSet1 = new PruebaWindowsForm.DB_A3E238_PersonasDBDataSet();
+            this.txt_cod = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_fecha = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_borrar = new System.Windows.Forms.Button();
+            this.btn_cancelar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personasBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dB_A3E238_PersonasDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dB_A3E238_PersonasDBDataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_agregar
+            // 
+            this.btn_agregar.Location = new System.Drawing.Point(12, 240);
+            this.btn_agregar.Name = "btn_agregar";
+            this.btn_agregar.Size = new System.Drawing.Size(61, 23);
+            this.btn_agregar.TabIndex = 4;
+            this.btn_agregar.Text = "Guardar";
+            this.btn_agregar.UseVisualStyleBackColor = true;
+            this.btn_agregar.Click += new System.EventHandler(this.btn_agregar_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nombre:";
+            // 
+            // txt_nom
+            // 
+            this.txt_nom.Location = new System.Drawing.Point(81, 77);
+            this.txt_nom.Name = "txt_nom";
+            this.txt_nom.Size = new System.Drawing.Size(147, 20);
+            this.txt_nom.TabIndex = 6;
+            // 
+            // txt_ap
+            // 
+            this.txt_ap.Location = new System.Drawing.Point(81, 117);
+            this.txt_ap.Name = "txt_ap";
+            this.txt_ap.Size = new System.Drawing.Size(148, 20);
+            this.txt_ap.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Apellido:";
+            // 
+            // txt_edad
+            // 
+            this.txt_edad.Location = new System.Drawing.Point(81, 157);
+            this.txt_edad.Name = "txt_edad";
+            this.txt_edad.Size = new System.Drawing.Size(148, 20);
+            this.txt_edad.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Edad";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.cod,
+            this.nombre,
+            this.apellido,
+            this.edad,
+            this.fechaalta});
+            this.dataGridView1.DataSource = this.personasBindingSource;
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle10.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle10.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle10;
+            this.dataGridView1.Location = new System.Drawing.Point(263, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 30;
+            this.dataGridView1.Size = new System.Drawing.Size(446, 206);
+            this.dataGridView1.TabIndex = 20;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // cod
+            // 
+            this.cod.DataPropertyName = "cod";
+            dataGridViewCellStyle9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.cod.DefaultCellStyle = dataGridViewCellStyle9;
+            this.cod.HeaderText = "cod";
+            this.cod.Name = "cod";
+            this.cod.ReadOnly = true;
+            this.cod.Width = 50;
+            // 
+            // nombre
+            // 
+            this.nombre.DataPropertyName = "nombre";
+            this.nombre.HeaderText = "nombre";
+            this.nombre.Name = "nombre";
+            this.nombre.Width = 67;
+            // 
+            // apellido
+            // 
+            this.apellido.DataPropertyName = "apellido";
+            this.apellido.HeaderText = "apellido";
+            this.apellido.Name = "apellido";
+            this.apellido.Width = 68;
+            // 
+            // edad
+            // 
+            this.edad.DataPropertyName = "edad";
+            this.edad.HeaderText = "edad";
+            this.edad.Name = "edad";
+            this.edad.Width = 56;
+            // 
+            // fechaalta
+            // 
+            this.fechaalta.DataPropertyName = "fecha_alta";
+            this.fechaalta.HeaderText = "fecha_alta";
+            this.fechaalta.Name = "fechaalta";
+            this.fechaalta.Width = 82;
+            // 
+            // personasBindingSource
+            // 
+            this.personasBindingSource.DataMember = "Personas";
+            this.personasBindingSource.DataSource = this.dB_A3E238_PersonasDBDataSet;
+            // 
+            // dB_A3E238_PersonasDBDataSet
+            // 
+            this.dB_A3E238_PersonasDBDataSet.DataSetName = "DB_A3E238_PersonasDBDataSet";
+            this.dB_A3E238_PersonasDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // personasTableAdapter
+            // 
+            this.personasTableAdapter.ClearBeforeFill = true;
+            // 
+            // dB_A3E238_PersonasDBDataSet1
+            // 
+            this.dB_A3E238_PersonasDBDataSet1.DataSetName = "DB_A3E238_PersonasDBDataSet";
+            this.dB_A3E238_PersonasDBDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // txt_cod
+            // 
+            this.txt_cod.Location = new System.Drawing.Point(81, 41);
+            this.txt_cod.Name = "txt_cod";
+            this.txt_cod.ReadOnly = true;
+            this.txt_cod.Size = new System.Drawing.Size(148, 20);
+            this.txt_cod.TabIndex = 22;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Cod:";
+            // 
+            // txt_fecha
+            // 
+            this.txt_fecha.Location = new System.Drawing.Point(81, 197);
+            this.txt_fecha.Name = "txt_fecha";
+            this.txt_fecha.Size = new System.Drawing.Size(148, 20);
+            this.txt_fecha.TabIndex = 24;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Fecha:";
+            // 
+            // btn_borrar
+            // 
+            this.btn_borrar.Location = new System.Drawing.Point(81, 238);
+            this.btn_borrar.Name = "btn_borrar";
+            this.btn_borrar.Size = new System.Drawing.Size(71, 26);
+            this.btn_borrar.TabIndex = 25;
+            this.btn_borrar.Text = "Borrar";
+            this.btn_borrar.UseVisualStyleBackColor = true;
+            this.btn_borrar.Click += new System.EventHandler(this.btn_borrar_Click);
+            // 
+            // btn_cancelar
+            // 
+            this.btn_cancelar.Location = new System.Drawing.Point(158, 237);
+            this.btn_cancelar.Name = "btn_cancelar";
+            this.btn_cancelar.Size = new System.Drawing.Size(71, 26);
+            this.btn_cancelar.TabIndex = 26;
+            this.btn_cancelar.Text = "Cancelar";
+            this.btn_cancelar.UseVisualStyleBackColor = true;
+            this.btn_cancelar.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(689, 310);
+            this.Controls.Add(this.btn_cancelar);
+            this.Controls.Add(this.btn_borrar);
+            this.Controls.Add(this.txt_fecha);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_cod);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txt_edad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_ap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_nom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_agregar);
+            this.Name = "Form1";
+            this.Text = "Gestor Win";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personasBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dB_A3E238_PersonasDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dB_A3E238_PersonasDBDataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_agregar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_nom;
+        private System.Windows.Forms.TextBox txt_ap;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_edad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DB_A3E238_PersonasDBDataSet dB_A3E238_PersonasDBDataSet;
+        private System.Windows.Forms.BindingSource personasBindingSource;
+        private DB_A3E238_PersonasDBDataSetTableAdapters.PersonasTableAdapter personasTableAdapter;
+        private DB_A3E238_PersonasDBDataSet dB_A3E238_PersonasDBDataSet1;
+        private System.Windows.Forms.TextBox txt_cod;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_fecha;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_borrar;
+        private System.Windows.Forms.Button btn_cancelar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cod;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn edad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fechaalta;
+    }
+}
+
---
+++ PruebaWindowsForm/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaWindowsForm
+{
+    public partial class Form1 : Form
+    {
+        DB_A3E238_PersonasDBEntities db = new DB_A3E238_PersonasDBEntities();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resetearCamposAgregar();
+            actualizarGrid();
+
+        }
+        private void actualizarGrid()
+        {
+            this.personasTableAdapter.Fill(this.dB_A3E238_PersonasDBDataSet.Personas);
+
+        }
+
+        private void resetearCamposAgregar()
+        {
+            txt_cod.Text = txt_nom.Text = txt_ap.Text = txt_edad.Text = "";
+            txt_fecha.Text = DateTime.Today.Date.ToShortDateString().ToString();
+
+            btn_agregar.Text = "Guardar";
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            resetearCamposAgregar();
+
+        }
+
+
+
+       
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+            if (dataGridView1.CurrentRow.Index != -1)
+            {
+                btn_agregar.Text = "Actualizar";
+                Personas persona = new Personas();
+                int cod = Convert.ToInt32(dataGridView1.CurrentRow.Cells["cod"].Value.ToString());
+
+                try
+                {
+
+                    persona = db.Personas.Find(cod);
+
+                    txt_cod.Text = persona.cod.ToString();
+                    txt_nom.Text = persona.nombre.ToString();
+                    txt_ap.Text = persona.apellido.ToString();
+                    txt_edad.Text = persona.edad.ToString();
+                    txt_fecha.Text = persona.fecha_alta.Value.ToShortDateString();
+                }
+                catch (Exception ex) { }
+            }
+        }
+
+        private void btn_borrar_Click(object sender, EventArgs e)
+        {
+            long cod = long.Parse(txt_cod.Text.ToString() );
+
+            db.Personas.Remove(db.Personas.Find(cod));
+            db.SaveChanges();
+            actualizarGrid();
+            resetearCamposAgregar();
+        }
+
+        private void btn_agregar_Click_1(object sender, EventArgs e)
+        {
+            //------------Agregar persona
+
+            Personas persona = new Personas();
+            if (btn_agregar.Text == "Actualizar")
+            {
+                persona.cod = long.Parse(txt_cod.Text.ToString());
+            }
+            try
+            {
+
+                persona.nombre = txt_nom.Text.ToString();
+                persona.apellido = txt_ap.Text.ToString();
+                persona.edad = Int32.Parse(txt_edad.Text.ToString());
+                persona.fecha_alta = DateTime.Today.Date;
+
+                db.Personas.Add(persona);
+                db.SaveChanges();
+
+                resetearCamposAgregar();
+                actualizarGrid();
+            }
+            catch (Exception ex)
+            {
+                btn_agregar.Text = "error";
+            }
+
+        }
+    }
+}
